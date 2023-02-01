4673fb2587e5c2601ed00fcb1569636bebc05820
Phan Anh Tu <okthichiatay77@gmail.com>
2023-02-01 21:19:34 +0700

    change database

--- analysis_SEO/analysis_data.py
+++ analysis_SEO/analysis_data.py
@@ -45,18 +45,20 @@ def handle_total(domain):
     return context
 
 
-def html_version(soup):
-    ...
-
 
 def handle_heading(soup):
     list_h1 = soup.find_all("h1")
     if len(list_h1) == 0:
-        result = 'Trang web của bạn không có thẻ h1.'
+        msg = 'Trang web của bạn không có thẻ h1.'
+        status = 'Error'
     elif len(list_h1) > 1:
-        result = 'Trang web của bạn có nhiều hơn một thẻ h1 để tối ưu bạn nên sử dụng một thẻ h1.'
+        msg = 'Trang web của bạn có nhiều hơn một thẻ h1 để tối ưu bạn nên sử dụng một thẻ h1.'
+        status = 'Warning'
     else:
-        result = 'Pass'
+        msg = 'Tốt. Trang web của bạn chỉ có một thẻ h1.'
+        status = 'Pass'
+
+    result = [status, msg]
 
     return result
 
@@ -65,28 +67,37 @@ def handle_title(soup):
     title = soup.find_all('title')
     if title:
         if len(title) > 1:
-            result = 'Trang web của bạn có nhiều hơn một thẻ meta title'
+            msg = 'Trang web của bạn có nhiều hơn một thẻ meta title'
+            status = 'Warning'
         elif len(title) == 0:
-            result = 'Trang web của bạn không có thẻ meta title'
+            msg = 'Trang web của bạn không có thẻ meta title'
+            status = 'Error'
         else:
             content = title[0].get_text()
             if len(content) > 100:
-                result = 'Nội dung của the meta title dài hơn 100 ký tự vui lòng tối ưu hóa lại'
+                msg = 'Nội dung của the meta title dài hơn 100 ký tự. Vui lòng kiểm tra lại'
+                status = 'Warning'
             else:
-                result = 'Pass'
+                msg = 'Tốt. Trang web của bạn thẻ title đúng yêu cầu'
+                status = 'Pass'
     else:
-        result = 'Trang web của bạn không có thẻ title'
+        msg = 'Trang web của bạn không có thẻ title'
+        status = 'Error'
 
+    result = [status, msg]
     return result
 
 
 def handle_conanical(soup):
     canonical = soup.find('link', attrs={'rel': 'canonical'})
     if canonical:
-        result = 'Pass'
+        msg = 'Tốt. Trang của bạn có thẻ meta conanical'
+        status = 'Pass'
     else:
-        result = 'Trang web của bạn không có thẻ Canonical'
+        msg = 'Trang web của bạn không có thẻ meta canonical'
+        status = 'Warning'
 
+    result = [status, msg]
     return result
 
 
@@ -94,68 +105,84 @@ def handle_meta_description(soup):
     meta_des = soup.find_all('meta', attrs={'name': 'description'})
 
     if len(meta_des) > 1:
-        result = 'Trang web của bạn có nhiều hơn một thẻ meta description.'
+        msg = 'Trang web của bạn có nhiều hơn một thẻ meta description.'
+        status = 'Warning'
     elif len(meta_des) == 0:
-        result = 'Trang web của bạn không có thẻ meta description.'
+        msg = 'Trang web của bạn không có thẻ meta description.'
+        status = 'Error'
     else:
-        result = 'Pass'
+        msg = 'Tốt. Trang web của bạn có thẻ meta description đúng yêu cầu'
+        status = 'Pass'
+
+    result = [status, msg]
     return result
 
 
 def meta_robots(soup):
-    result = ''
     check = soup.find_all('meta', attrs={'name': 'robots'})
     if check:
         if len(check) > 1:
-            result = 'Trang web của bạn có thẻ meta robots trùng lặp'
+            msg = 'Trang web của bạn có thẻ meta robots trùng lặp'
+            status = 'Warning'
         else:
-            result = 'Pass'
+            msg = 'Tốt. Trang web của bạn có thẻ meta robots theo yêu cầu'
+            status = 'Pass'
     else:
-        result = 'Trang web của bạn không có thẻ meta robots'
+        msg = 'Trang web của bạn không có thẻ meta robots'
+        status = 'Error'
 
+    result = [status, msg]
     return result
 
 
 def meta_revisit_after(soup):
-    result = ''
     check = soup.find_all('meta', attrs={'name': 'revisit-after'})
     if check:
         if len(check) > 1:
-            result = 'Trang web của bạn có thẻ meta revisit-after trùng lặp'
+            msg = 'Trang web của bạn có thẻ meta revisit-after trùng lặp'
+            status = 'Warning'
         else:
-            result = 'Pass'
+            msg = 'Tốt. Trang web của bạn có thẻ đúng yêu cầu'
+            status = 'Pass'
     else:
-        result = 'Trang web của bạn không có thẻ meta revisit-after'
+        msg = 'Trang web của bạn không có thẻ meta revisit-after'
+        status = 'Error'
 
+    result = [status, msg]
     return result
 
 
 def content_language(soup):
-    result = ''
     check = soup.find_all('meta', attrs={'name': 'content-language'})
     if check:
         if len(check) > 1:
-            result = 'Trang web của bạn có thẻ meta content-language trùng lặp'
+            msg = 'Trang web của bạn có thẻ meta content-language trùng lặp'
+            status = 'Warning'
         else:
-            result = 'Pass'
+            msg = 'Tốt. Trang web của bạn có thẻ content-language'
+            status = 'Pass'
     else:
-        result = 'Trang web của bạn không có thẻ meta content-language'
+        msg = 'Trang web của bạn không có thẻ meta content-language'
+        status = 'Error'
 
+    result = [status, msg]
     return result
 
 
 def content_type(soup):
-    result = ''
-
     check = soup.find_all('meta', attrs={'http-equiv': 'content-type'})
     if check:
         if len(check) > 1:
-            result = 'Trang web của bạn có thẻ meta content-type trùng lặp'
+            msg = 'Trang web của bạn có thẻ meta content-type trùng lặp'
+            status = 'Warning'
         else:
-            result = 'Pass'
+            msg = 'Tốt. Trang web của bạn có thẻ content-type'
+            status = 'Pass'
     else:
-        result = 'Trang web của bạn không có thẻ meta content-type'
+        msg = 'Trang web của bạn không có thẻ meta content-type'
+        status = 'Error'
 
+    result = [status, msg]
     return result
 
 
@@ -163,17 +190,20 @@ def meta_viewport(soup):
     check = soup.find_all('meta', attrs={'name': 'viewport'})
     if check:
         if len(check) > 1:
-            result = 'Trang web của bạn có thẻ meta viewport trùng lặp'
+            msg = 'Trang web của bạn có thẻ meta viewport trùng lặp'
+            status = 'Warning'
         else:
-            result = 'Pass'
+            msg = 'Tốt. Trang web của bạn có thẻ viewport'
+            status = 'Pass'
     else:
-        result = 'Trang web của bạn không có thẻ meta viewport'
+        msg = 'Trang web của bạn không có thẻ meta viewport'
+        status = 'Error'
 
+    result = [status, msg]
     return result
 
 
 def favicon(soup):
-    result = ''
     check_icon = 0
     for link in soup.head.find_all('link'):
         if 'icon' in link['rel']:
@@ -181,24 +211,33 @@ def favicon(soup):
             break
 
     if check_icon == 0:
-        result = 'Trang web của bạn không có favicon'
-    elif check_icon == 1:
-        result = 'Pass'
+        msg = 'Trang web của bạn không có favicon'
+        status = 'Error'
+    else:
+        msg = 'Tốt. Trang web của bạn có icon'
+        status = 'Pass'
 
+    result = [status, msg]
     return result
 
 
 def image_alt(soup):
-    result = 'Pass'
-
+    check = 0
     list_img = soup.find_all('img', src=True)
     for img in list_img:
         try:
             img['alt']
         except:
-            if result == 'Pass':
-                result = 0
-            result += 1
+            check += 1
+
+    if check > 0:
+        msg = 'Trang web của bạn có ' + str(check) + ' thẻ không có thuộc tính alt'
+        status = 'Warning'
+    else:
+        msg = 'Tốt trang của bạn tất cả thẻ img có thuộc tính alt'
+        status = 'Pass'
+
+    result = [status, msg]
 
     return result
 
@@ -215,10 +254,13 @@ def handle_sitemap(domain):
     else:
         status_code = requests.get(domain + '/sitemap.xml').status_code
     if status_code != 200:
-        result = 'Trang web của bạn không có file sitemap.xml'
+        msg = 'Trang web của bạn không có file sitemap.xml'
+        status = 'Error'
     else:
-        result = 'Pass'
+        msg = 'Tốt. Trang của bạn có file sitemap'
+        status = 'Pass'
 
+    result = [status, msg]
     return result
 
 
